b40df87577a1c3295507b8ddaac50b8a3585e546
subbu20204 <subbu20204@gmail.com>
2024-03-08 10:52:29 +0530

    initial state

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,30 @@
+
+export function Hero() {
+  return (
+    <div className="pt-4 lg:pt-10">
+      <div className="px-[20px] lg:px-[80px]">
+        <h1 className="text-left text-[32px] leading-[40px] font-medium text-[#172026] lg:text-[150px] lg:leading-[150px]">
+          Find 
+        </h1>
+        <h1 className="text-left text-[32px] leading-[40px] font-medium text-[#172026] lg:text-[150px] lg:leading-[150px]">
+            Your  
+        </h1>
+        <h1 className="text-left text-[32px] leading-[40px] font-medium text-[#172026] lg:text-[150px] lg:leading-[150px]">
+            Perfect Match
+        </h1>
+        
+          
+        <p className="text-start pt-6 text-[#36485C] lg:text-[30px] lg:leading-7px">
+          No more roomate Drama..
+        </p>
+
+        <div className="flex w-full pt-8 justify-center gap-x-6 ">
+          <button className="bg-[#4328EB] w-1/2 py-4 px-8 text-white rounded-[4px] lg:w-fit">
+            <p>click</p>
+          </button>
+
+        </div>
+      </div>
+    </div>
+  );
+}
